78c915eb1ca600a9de699631a1881578833c0175
Hillarys camilo <83847636+hillaryscamilo19@users.noreply.github.com>
2023-04-12 12:44:04 -0400

    pipe-filtros

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -10,11 +10,24 @@ import { TracksModules } from '@core/model/tracks.model';
 })
 export class PlayListBodyComponent implements OnInit{
   tracks: TracksModules [] = []
+  optionSort: { property: string | null, order: string } = { property: null, order: 'asc' }
+
   constructor(){}
 
   ngOnInit(): void {
     const { data }: any = (dataRaw as any).default
     this.tracks = data
+  }
+
+
+  ChangeSort(property:string):void{
+    const {order} = this.optionSort
+    this.optionSort = {
+      property,
+      order: order === 'asc' ? 'desc' : 'asc'
+    }
+    console.log(this.optionSort);
+
   }
 
 }
--- src/app/shared/pipe/order-list.pipe.ts
+++ src/app/shared/pipe/order-list.pipe.ts
@@ -1,6 +1,6 @@
-import { TracksModules } from './../../core/model/tracks.model';
+ import { TracksModules } from './../../core/model/tracks.model';
 import { Pipe, PipeTransform } from '@angular/core';
-import { v4 as uuidv4} from 'uuid';
+// import { v4 as uuidv4} from 'uuid';
 
 @Pipe({
   name: 'orderList'
